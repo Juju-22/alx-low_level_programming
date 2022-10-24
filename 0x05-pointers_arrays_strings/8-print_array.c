be6bcaf42b5b9beb2d988cfaae580ab36586f897
Juju-22 <113732989+Juju-22@users.noreply.github.com>
2022-10-24 09:14:10 -1100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+#include <std.io>
 
 /**
- * print_array - input num of array
- * @a: array
- * @n: elements to print
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
